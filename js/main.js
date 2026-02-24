04336b38b66dc133a2a7dbd2e3a1bb774ca4cdcf
Umidjon <nizomovanilufarxon17@gmail.com>
2026-02-24 16:48:44 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,145 @@
+var optimus = prompt("Ismingizni kiriting:");
+var glacier = prompt("Familiyangizni kiriting:");
+
+var slash = prompt("Telefon raqamingizni kiriting:");
+
+console.log(optimus, glacier, script, iron, slash);
+//===========================
+var num1 = 5;
+var num2 = 7;
+
+//===========================
+
+var yegindi = num1 + num2;
+//===========================
+
+var ayirma = num1 - num2;
+//===========================
+
+var kopaytiruv = num1 * num2;
+//===========================
+
+var boluv = num1 / num2;
+console.log(yegindi);
+console.log();
+//===========================
+
+console.log(ayirma);
+console.log();
+//===========================
+
+console.log(kopaytiruv);
+console.log();
+//============================
+
+console.log(boluv);
+console.log();
+//============================
+
+var num3 = 7;
+var kvadrat = num3 * num3;
+console.log(kvadrat);
+console.log();
+//============================
+
+var num4 = 8;
+var kvadrat2 = Math.pow(num4, 2);
+console.log(kvadrat2);
+console.log();
+//===========================
+
+var num5 = 14;
+var kvadrat3 = num5 * num5 * num5;
+console.log(kvadrat3);
+console.log();
+//============================
+
+var num6 = 12;
+var perimetr = num6 * 4;
+console.log(perimetr);
+console.log();
+//=============================
+
+var num7 = 22;
+var yuza = num7 * num7;
+console.log(yuza);
+console.log();
+//============================
+
+var num8 = 11;
+var num9 = 17;
+var perimetr2 = 2 * (num8 + num9);
+console.log(perimetr2);
+console.log();
+//============================
+
+var num10 = 30;
+var num11 = 27;
+var yuza2 = 2 * (num10 + num11);
+console.log(yuza2);
+console.log();
+//============================
+
+var num12 = 32;
+var num13 = 23;
+var s = num12 / num13;
+console.log(s);
+console.log();
+//===========================
+
+var num14 = 65;
+var num15 = 44;
+var t = num14 / num15;
+console.log(t);
+console.log();
+//===========================
+
+var num16 = 70;
+var num17 = 55;
+var v = num16 * num17;
+console.log(v);
+console.log();
+//===========================
+
+var num18 = 39;
+var num19 = 77;
+var ortacha = 2 / (num18 + num19);
+console.log(ortacha);
+console.log();
+//============================
+
+var num20 = 59;
+var num21 = 47;
+var num22 = 57;
+var ortacha2 = 3 / (num20 + num21 + num22);
+console.log(ortacha2);
+console.log();
+//============================
+
+var num23 = 2026;
+var num24 = 1999;
+var yil = num23 - num24;
+console.log(yil);
+console.log();
+//============================
+
+var soat = 7;
+var minut = 60;
+var topish = soat * minut;
+console.log(topish);
+console.log();
+//============================
+
+var sekund = 60;
+var minut2 = 42;
+var topish2 = sekund * minut2;
+console.log(topish2);
+console.log();
+//============================
+
+var pul = 13982;
+var odam = 232;
+var umumiy = pul / odam;
+console.log(umumiy);
+console.log();
+//============================
